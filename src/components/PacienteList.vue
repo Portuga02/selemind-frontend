c3f3e585ce4f8ef72404e9444ab753d58c9c5dea
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2025-06-04 19:42:13 -0300

    Criação e ajustes de rotas

---
+++ src/components/PacienteList.vue
@@ -0,0 +1,21 @@
+<template>
+   <b-container>
+       <b-card title="Lista de Pacientes">
+           <b-table striped hover :items="pacientes" :fields="campos"></b-table>
+       </b-card>
+   </b-container>
+</template>
+
+<script>
+export default {
+   data() {
+       return {
+           campos: ['id', 'nome', 'idade'],
+           pacientes: [
+               { id: 1, nome: 'Carlos Silva', idade: 40 },
+               { id: 2, nome: 'Ana Souza', idade: 28 }
+           ]
+       };
+   }
+};
+</script>
---
+++ src/components/PacientesForm.vue
@@ -0,0 +1,32 @@
+<template>
+   <b-container>
+       <b-card title="Cadastrar Paciente">
+           <b-form @submit.prevent="salvarPaciente">
+               <b-form-group label="Nome:" label-for="nome">
+                   <b-form-input id="nome" v-model="paciente.nome" required></b-form-input>
+               </b-form-group>
+
+               <b-form-group label="Idade:" label-for="idade">
+                   <b-form-input id="idade" type="number" v-model="paciente.idade"></b-form-input>
+               </b-form-group>
+
+               <b-button type="submit" variant="primary">Salvar</b-button>
+           </b-form>
+       </b-card>
+   </b-container>
+</template>
+
+<script>
+export default {
+   data() {
+       return {
+           paciente: { nome: '', idade: null }
+       };
+   },
+   methods: {
+       salvarPaciente() {
+           console.log("Paciente salvo:", this.paciente);
+       }
+   }
+};
+</script>
